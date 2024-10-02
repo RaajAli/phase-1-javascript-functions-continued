5114b0134a3aaa0a83bffae5c71c5ffba7c76faf
Raaj Ali <raajrali1999@gmail.com>
2024-10-02 16:17:56 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(activity) {
+    if (activity === undefined) {
+        activity = 'roller-skate';
+    }
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity) {
+    if (activity === undefined) {
+        activity = 'go to the office';
+    }
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(visualFlair = "*") {
+    return function(adj = "special") {
+        return `You are ${visualFlair}${adj}${visualFlair}!`;
+    };
+}
